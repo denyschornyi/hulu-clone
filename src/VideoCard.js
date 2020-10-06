38a745da7cb3d65cef63fd3e6749a7aee9aba589
Denys Chornyi <chornijj14@gmail.com>
2020-10-06 21:14:25 +0200

    Movie card get filled by API data

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import TextTruncate from 'react-text-truncate';
 
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+
 import './VideoCard.css'
 
 
 const base_url = 'https://image.tmdb.org/t/p/original'
 
 function VideoCard({movie}) {
-    console.log(movie);
     return (
         <div className="videoCard">
             <img src={base_url + movie.backdrop_path || movie.poster_path} alt="movie"/>
@@ -18,7 +19,12 @@ function VideoCard({movie}) {
                 text={movie.overview}
             />
             <h2>{movie.title || movie.original_name}</h2>
-            <p>{movie.first_air_date || movie.release_date}</p>
+
+            <p className="videoCard__stats">
+                {movie.release_date || movie.first_air_date}•
+                <ThumbUpIcon/>
+                {movie.vote_count}
+            </p>            
         </div>
     )
 }
