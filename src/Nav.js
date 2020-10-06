6ce0af55e164dec8b07ab10c899a3eb29ec6701b
Denys Chornyi <chornijj14@gmail.com>
2020-10-06 22:18:36 +0200

    requset for each category

--- src/Nav.js
+++ src/Nav.js
@@ -10,15 +10,15 @@ function Nav({setSelectedOption}) {
         <div className="nav">
             <h2 onClick={() => setSelectedOption(requests.fetchTrending)}>Trending</h2>
             <h2 onClick={() => setSelectedOption(requests.fetchTopRated)}>Top Rated</h2>
-            <h2>Action</h2>
-            <h2>Comedy</h2>
-            <h2>Horor</h2>
-            <h2>Romance</h2>
-            <h2>Mystery</h2>
-            <h2>Sci-fi</h2>
-            <h2>Western</h2>
-            <h2>Animation</h2>
-            <h2>TV Movie</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>Action</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchComedyMovies)}>Comedy</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchHorrorMovie)}>Horor</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchRomanceMovies)}>Romance</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchMystery)}>Mystery</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchSciFi)}>Sci-fi</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchWestern)}>Western</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchAnimation)}>Animation</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchTV)}>TV Movie</h2>
         </div>
     )
 }
--- src/requests.js
+++ src/requests.js
@@ -2,5 +2,14 @@ const API_KEY = 'a50cde705e9b73389fbfaad0fd645e4f';
 
 export default {
     fetchTrending : `/trending/all/week?api_key=${API_KEY}`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovie: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+    fetchTV: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
 }
